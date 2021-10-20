5a7812f25550c9984ad083246ecb46634445ae24
Ronald Bett <rbett@jambopay.com>
2021-10-20 12:42:18 +0300

    Complete

--- RemoveDublicatesInLinkedList/Program.cs
+++ RemoveDublicatesInLinkedList/Program.cs
@@ -13,17 +13,31 @@ namespace RemoveDublicatesInLinkedList
             this.next = next;
         }
     }
-    
-    public class Solution {
-        public ListNode DeleteDuplicates(ListNode head)
-        {
 
+    public class Solution
+    {
+        public static ListNode DeleteDuplicates(ListNode head)
+        {
             var dummyNode = new ListNode(0) {next = head};
 
             var prev = dummyNode;
 
-            while (head!=null)
+            while (head != null)
             {
+                if (head.next != null && head.val == head.next.val)
+                {
+                    while (head.next != null && head.val == head.next.val)
+                    {
+                        head = head.next;
+                    }
+
+                    prev.next = head.next;
+                }
+                else
+                {
+                    prev = prev.next;
+                }
+
                 head = head.next;
             }
 
@@ -35,7 +49,16 @@ namespace RemoveDublicatesInLinkedList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //[1,2,3,3,4,4,5]
+            var head = new ListNode(1,
+                new ListNode(2, new ListNode(3, new ListNode(3, new ListNode(4, new ListNode(4, new ListNode(5)))))));
+            var res = Solution.DeleteDuplicates(head);
+            var curr = res;
+            while (curr != null)
+            {
+                Console.WriteLine(curr.val);
+                curr = curr.next;
+            }
         }
     }
 }
